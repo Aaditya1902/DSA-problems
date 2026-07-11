c8cd0a390f246216c2c1de357d6a9a91d41defe2
Aaditya Singh Rawat <aadityarawat1902@gmail.com>
2026-07-11 12:37:20 +0530

    Time: 24 ms (77.34%) | Memory: 48.5 MB (86.91%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,12 +1,15 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         
+        // HashMap will store all the prefixSum upto the index <Sum, Frequency>
         HashMap<Integer,Integer> hm = new HashMap<>();
 
-        int prefixsum=0;
+        int prefixsum=0;// Actual Sum upto the Index
 
         int count=0;
 
+// If prefixSum - k = 0, it means the subarray starts from index 0.
+        // We add (0, 1) to handle this case naturally.
         hm.put(0,1);
 
         for(int i=0;i<nums.length;i++){
@@ -14,10 +17,12 @@ class Solution {
 
             int remove = prefixsum-k;
 
+// If HashMap contains the exact prefixSum to remove, it means we found valid subarrays
             if(hm.containsKey(remove)){
                 count+=hm.get(remove);
             }
 
+// Store/Update the current prefixSum frequency in the map
             hm.put(prefixsum, hm.getOrDefault(prefixsum,0)+1);
         }
         return count;
