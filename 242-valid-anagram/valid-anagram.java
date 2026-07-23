2c57aa30d0cffcef449a9ef7ff2fa5ad0a2df460
Aaditya Singh Rawat <aadityarawat1902@gmail.com>
2026-07-23 12:00:35 +0530

    Time: 4 ms (82.35%) | Memory: 46.6 MB (16.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,21 +4,18 @@ class Solution {
             return false;
         }
 
-        Map<Character, Integer> counter = new HashMap<>();
+        
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            counter.put(ch, counter.getOrDefault(ch, 0) + 1);
+        char[] str1=s.toCharArray();
+        char[] str2=t.toCharArray();
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        if(Arrays.equals(str1,str2)){
+            return true;
+        }else{
+            return false;
         }
-
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            if (!counter.containsKey(ch) || counter.get(ch) == 0) {
-                return false;
-            }
-            counter.put(ch, counter.get(ch) - 1);
-        }
-
-        return true;
     }
 }
