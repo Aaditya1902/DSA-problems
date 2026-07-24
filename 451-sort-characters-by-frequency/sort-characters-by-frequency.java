e445b7e08b32fb38ccbd8c8bee4eef04c0a99975
Aaditya Singh Rawat <aadityarawat1902@gmail.com>
2026-07-24 22:48:59 +0530

    Time: 3 ms (99.78%) | Memory: 46.5 MB (65.30%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,23 +1,33 @@
 class Solution {
     public String frequencySort(String s) {
-         Map<Character, Integer> freq = new HashMap<>();
-        
+         int[] freq = new int[128];
         for (char c : s.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+            freq[c]++;
         }
-        
-        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
-            (a, b) -> b.getValue() - a.getValue()
-        );
-        
-        pq.addAll(freq.entrySet());
-        
-        StringBuilder result = new StringBuilder();
-        while (!pq.isEmpty()) {
-            Map.Entry<Character, Integer> entry = pq.poll();
-            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+
+        //         What it does: It creates a frequency array of size 128 (which covers all standard ASCII characters).
+        // The Trick: In Java, a char is secretly just a number under the hood (e.g., 'a' is 97, 'b' is 98). By writing freq[c]++, Java automatically converts the character into its ASCII numeric index. If the string contains 'a', freq[97] increases by 1.
+
+        char[] res = new char[s.length()];
+        int index = 0;
+
+        while (index < s.length()) {
+            int max = 0;
+            int maxCharIdx = 0;
+
+            for (int i = 0; i < 128; i++) {
+                if (freq[i] > max) {
+                    max = freq[i];
+                    maxCharIdx = i;
+                }
+            }
+
+            while (freq[maxCharIdx] > 0) {
+                res[index++] = (char) maxCharIdx;
+                freq[maxCharIdx]--;
+            }
         }
-        
-        return result.toString();
+
+        return new String(res);
     }
 }
