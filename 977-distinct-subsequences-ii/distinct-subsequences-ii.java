42dc23a5c38e71987e124ea013b10f0ad5e1a976
Aaditya Singh Rawat <aadityarawat1902@gmail.com>
2026-09-20 16:45:14 +0530

    Time: 2 ms (99.80%) | Memory: 43.1 MB (65.03%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.java
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -1,24 +1,15 @@
 class Solution {
-    int q = 1000000007;
     public int distinctSubseqII(String s) {
-        int[] hash = new int[26]; //to track previously used same character
-        Arrays.fill(hash,-1); // all characters are intitally not used
-        int[] arr = new int[s.length()+1];
-        arr[0]=1;
-        for(int i=0;i<s.length();i++){
-            if(hash[s.charAt(i)-'a']==-1){
-                arr[i+1]=(arr[i]+arr[i])%q;// add num of subsequence that can be formed using i-th character
-                hash[s.charAt(i)-'a']=i;
-            }else{
-                arr[i+1]=(arr[i]+arr[i])%q;
-                arr[i+1]-=arr[hash[s.charAt(i)-'a']]; // if i-th character is repeatition of previous charcter then subtract the number of subsequence that you added previously as that number of subsequeces are not distinct 
-                if(arr[i+1]<0){
-                    arr[i+1]+=q;
-                }
-                hash[s.charAt(i)-'a']=i;
-            }
+        int MOD = 1_000_000_007;
+
+        long dp=1;
+        long[] last = new long[26];
+        for(char c: s.toCharArray()){
+            int idx = c - 'a';
+            long newDp = (2*dp% MOD - last[idx] + MOD) % MOD;
+            last[idx] = dp;
+            dp=newDp;
         }
-        // System.out.println(Arrays.toString(arr));
-        return arr[arr.length-1]-1<0?arr[arr.length-1]-1+q:arr[arr.length-1]-1;
+        return (int) ((dp-1+MOD)%MOD);
     }
 }
