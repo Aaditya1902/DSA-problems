adb028ff28fd2539a9a546566f3cca90acc0be09
Aaditya Singh Rawat <aadityarawat1902@gmail.com>
2026-09-23 01:37:42 +0530

    Time: 109 ms (94.28%) | Memory: 43.2 MB (18.27%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,42 +1,39 @@
-public class Solution {
+class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
+        char[] words=word.toCharArray();
 
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]==words[0] && recur(board,i,j,words,0)){
+                    return true;
                 }
             }
+
         }
-        
         return false;
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
+
+    private boolean recur(char[][] board,int i,int j,char[] words,int idx){
+        if(idx==words.length){
             return true;
         }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j]!=words[idx]  ||  board[i][j]=='*'){
             return false;
         }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
+
+        char temp=board[i][j];
+        board[i][j]='*';
+
+        if(recur(board,i+1,j,words,idx+1) || 
+            recur(board,i-1,j,words,idx+1) || 
+            recur(board,i,j+1,words,idx+1) || 
+            recur(board,i,j-1,words,idx+1)  ){
+                return true;
+            }
+
+        board[i][j]=temp;
+
         return false;
     }
 }
