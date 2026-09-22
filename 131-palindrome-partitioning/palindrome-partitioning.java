a0248874302f733832cd448e4d07c88d53390c92
Aaditya Singh Rawat <aadityarawat1902@gmail.com>
2026-09-23 00:59:21 +0530

    Time: 8 ms (73.59%) | Memory: 65.3 MB (40.50%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,30 +1,36 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        backtrack(s, 0, new ArrayList<>(), result);
-        return result;
+        List<List<String>> res = new ArrayList<>();
+
+        part(s,0,res,new ArrayList<>());
+
+        return res;
     }
 
-    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
-        if (start == s.length()) {
-            result.add(new ArrayList<>(path));
-            return;
-        }
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (isPalindrome(s, start, end - 1)) {
-                path.add(s.substring(start, end));
-                backtrack(s, end, path, result);
-                path.remove(path.size() - 1);
-            }
-        }
-    }
-
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left++) != s.charAt(right--)) {
+    boolean isPalindrome(String s, int start, int end){
+        while(start<end){
+            if(s.charAt(start) != s.charAt(end)){   
                 return false;
             }
+
+            start++;
+            end--;
         }
         return true;
+    }
+
+    void part(String s,int start,List<List<String>> res, List<String> temps){
+        if(start==s.length()){
+            res.add(new ArrayList<>(temps));
+            return;
+        }
+
+        for(int end=start+1;end<=s.length();end++){
+            if(isPalindrome(s,start,end-1)){
+                temps.add(s.substring(start,end));
+                part(s,end,res,temps);
+                temps.remove(temps.size()-1);
+            }
+        }
     }
 }
