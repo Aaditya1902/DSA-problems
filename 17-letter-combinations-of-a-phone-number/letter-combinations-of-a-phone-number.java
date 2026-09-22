4c2ab74e8c26582e6ec32d80e121e8afe6531cfb
Aaditya Singh Rawat <aadityarawat1902@gmail.com>
2026-09-22 16:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (79.97%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,37 +1,41 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+
+        
         List<String> res = new ArrayList<>();
-        
-        if (digits == null || digits.length() == 0) {
-            return res;
+
+        if(digits==null || digits.length()==0){
+            return  res;
         }
-        
-        Map<Character, String> digitToLetters = new HashMap<>();
-        digitToLetters.put('2', "abc");
-        digitToLetters.put('3', "def");
-        digitToLetters.put('4', "ghi");
-        digitToLetters.put('5', "jkl");
-        digitToLetters.put('6', "mno");
-        digitToLetters.put('7', "pqrs");
-        digitToLetters.put('8', "tuv");
-        digitToLetters.put('9', "wxyz");
-        
-        backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
-        
-        return res;        
+
+        Map<Character,String> map = new HashMap<>();
+
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+
+        generatestring(digits,0,res,new StringBuilder(),map);
+
+        return res;
     }
 
-    private void backtrack(String digits, int idx, StringBuilder comb, List<String> res, Map<Character, String> digitToLetters) {
-        if (idx == digits.length()) {
+    void generatestring(String digits, int idx, List<String> res, StringBuilder comb, Map<Character,String> map){
+        if(idx==digits.length()){
             res.add(comb.toString());
-            return;
+            return ;
         }
         
-        String letters = digitToLetters.get(digits.charAt(idx));
-        for (char letter : letters.toCharArray()) {
+
+        String letters  = map.get((digits.charAt(idx)));
+        for(char letter : letters.toCharArray()){
             comb.append(letter);
-            backtrack(digits, idx + 1, comb, res, digitToLetters);
-            comb.deleteCharAt(comb.length() - 1);
+            generatestring(digits,idx+1,res,comb,map);
+            comb.deleteCharAt(comb.length()-1);
         }
     }
 }
